ed62950417de98832a8c4637d19ddb569a53b10b
mariog <mario.gualsaquigomez@kaust.edu.sa>
2025-09-23 16:48:36 +0300

    Exercise 27 -  Approach using Objects programming. DATA UPLOAD, ROSALIND PASS

--- Problem27/Code27.py
+++ Problem27/Code27.py
@@ -1,3 +1,6 @@
+import glob
+
+
 class Node:
     def __init__(self, value):
         self.value = value  # Unique identifier or data for the node
@@ -42,7 +45,7 @@ class Graph:
         for node_value, node_obj in nodes.items():
             neighbor_values = sorted([n.value for n in node_obj.neighbors])
             if neighbor_values:
-                graph_repr += f"  {node_value} ->  {", ".join(neighbor_values)}\n"
+                graph_repr += f"{node_value} -> {",".join(neighbor_values)}\n"
         return graph_repr
 
 def DeBruijn(Text, k):
@@ -51,6 +54,51 @@ def DeBruijn(Text, k):
         Debruijn_graph.add_edge(Text[i:i+k-1], Text[i+1:i+k])
     return Debruijn_graph
 
-Text = "AAGATTCTCTAC"
-k = 4
-print(DeBruijn(Text, k))
+# Text = "AAGATTCTCTAC"
+# k = 4
+# print(DeBruijn(Text, k))
+
+################### EVAL FUCTION ###########################
+#Testing with files
+def read_file_txt(file_path):
+    try:
+        with open(file_path, "r") as file:
+            content = file.readlines()
+            return content
+    except FileNotFoundError:
+        print(f"Error File not found at {file_path}")
+    except Exception as error:
+        print(f"Error while reading file {file_path}: {error}")
+
+
+def write_file_txt(file_path, content):
+    name_split = file_path.split("/")
+    output_name = f"./outputs/{name_split[-1].strip(".txt")}_output.txt"
+    with open(output_name, "w") as f:
+        match content:
+            case str():
+                print(content, end="", file=f)
+            case int():
+                print(str(content), end="", file=f)
+            case list():
+                for text in content:
+                    print(str(text), end="\n", file=f)
+            case set():
+                for text in content:
+                    print(str(text), end="\n", file=f)
+            case dict():
+                for key in content:
+                    print(str(key), end="\n", file=f)
+
+
+# Getting txt files
+folder_path = "./inputs"
+input_files = glob.glob(f"{folder_path}/*.txt")
+
+# #MODIFY THIS SECTION FOR EACH FUNCTION
+for input_file in input_files:
+    file_load = read_file_txt(input_file)
+    k = int(file_load[0].strip("\n"))
+    Text = file_load[1].strip("\n")
+    graph = DeBruijn(Text, k)
+    write_file_txt(input_file, graph.__repr__())
